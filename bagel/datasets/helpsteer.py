db719f7b8fe875856dfb79968d2581bf9de515da
Jon Durbin <jon@jondurbin.com>
2023-12-03 05:41:35 -0500

    HelpSteer DPO data.

---
+++ bagel/datasets/helpsteer.py
@@ -0,0 +1,70 @@
+from loguru import logger
+from datasets import load_dataset, Dataset
+from bagel.datasets.util import get_uid, has_refusal
+
+CONFIDENCE = 2
+
+
+def load_data():
+    """HelpSteer dataset for DPO."""
+    logger.info("Loading HelpSteer dataset...")
+    dataset = load_dataset("nvidia/HelpSteer", split="train").filter(
+        lambda item: not has_refusal(item["response"])
+    )
+
+    # Use the highest correctness items as the "chosen" responses.
+    logger.info("Generating DPO pairs...")
+    options = {}
+    for item in dataset:
+        uid = get_uid(item["prompt"])
+        if uid not in options:
+            options[uid] = []
+        item["score"] = (
+            item["helpfulness"]
+            + item["correctness"]
+            + item["coherence"]
+            + item["complexity"]
+            + item["verbosity"]
+        )
+        options[uid].append(item)
+
+    # Select the chosen and rejected items.
+    data = []
+    for uid, responses in options.items():
+        if len(responses) < 2:
+            continue
+        chosen = None
+        chosen_score = 0
+        rejected = None
+        rejected_score = 0
+        for item in responses:
+            if item["correctness"] == 4:
+                if chosen:
+                    if item["score"] > chosen["score"]:
+                        rejected = chosen["response"]
+                        rejected_score = chosen["score"]
+                        chosen = item
+                        chosen_score = item["score"]
+                        break
+                else:
+                    chosen = item
+                    chosen_score = item["score"]
+            else:
+                if not rejected:
+                    rejected = item["response"]
+                    rejected_score = item["score"]
+        if chosen and rejected:
+            logger.success(f"Found DPO pair: {chosen_score} vs {rejected_score}")
+            data.append(
+                {
+                    "id": uid,
+                    "chosen": chosen,
+                    "rejected": rejected,
+                    "conversations": None,
+                }
+            )
+    return Dataset.from_list(data)
+
+
+if __name__ == "__main__":
+    print(load_data())
