4cfa4f0004f7cb4de9f07a9583a82143298e2090
Jon Durbin <jon@jondurbin.com>
2023-12-22 16:45:31 -0500

    Bump version.

--- bagel/data.py
+++ bagel/data.py
@@ -293,28 +293,28 @@ def format_io(tokenizer, dataset):
 
 def get_or_create_datasets(tokenizer):
     """Get or create the SFT/DPO datasets."""
-    if os.path.exists("bagel-input-output-v0.1.parquet"):
+    if os.path.exists("bagel-input-output-v0.3.parquet"):
         return (
-            Dataset.from_parquet("bagel-input-output-v0.1.parquet"),
-            Dataset.from_parquet("bagel-dpo-v0.1.parquet"),
+            Dataset.from_parquet("bagel-input-output-v0.3.parquet"),
+            Dataset.from_parquet("bagel-dpo-v0.3.parquet"),
         )
     dataset = None
-    if os.path.exists("bagel-clean-v0.1.parquet"):
-        dataset = Dataset.from_parquet("bagel-clean-v0.1.parquet")
+    if os.path.exists("bagel-clean-v0.3.parquet"):
+        dataset = Dataset.from_parquet("bagel-clean-v0.3.parquet")
     else:
-        if os.path.exists("bagel-raw-v0.1.parquet"):
-            dataset = decontaminate(Dataset.from_parquet("bagel-raw-v0.1.parquet"))
-            dataset.to_parquet("bagel-clean-v0.1.parquet")
+        if os.path.exists("bagel-raw-v0.3.parquet"):
+            dataset = decontaminate(Dataset.from_parquet("bagel-raw-v0.3.parquet"))
+            dataset.to_parquet("bagel-clean-v0.3.parquet")
         else:
             raw_dataset = load_datasets()
-            raw_dataset.to_parquet("bagel-raw-v0.1.parquet")
+            raw_dataset.to_parquet("bagel-raw-v0.3.parquet")
             dataset = decontaminate(raw_dataset)
-            dataset.to_parquet("bagel-clean-v0.1.parquet")
+            dataset.to_parquet("bagel-clean-v0.3.parquet")
 
     # Split the raw dataset into SFT data and DPO data.
     sft, dpo = format_io(tokenizer, dataset)
-    sft.to_parquet("bagel-input-output-v0.1.parquet")
-    dpo.to_parquet("bagel-dpo-v0.1.parquet")
+    sft.to_parquet("bagel-input-output-v0.3.parquet")
+    dpo.to_parquet("bagel-dpo-v0.3.parquet")
     return sft, dpo
 
 
